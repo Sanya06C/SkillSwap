de45d3e87f5f0a7134041f43c531f0e18edddb32
SiyaAgrawal95 <siyaagrawal9506@gmail.com>
2025-12-23 12:36:18 +0530

    ss

--- script.js
+++ script.js
@@ -11,6 +11,8 @@ const firebaseConfig = {
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
 const auth = firebase.auth();
+const db = firebase.firestore();
+
 
 // Login function
 function login() {
@@ -25,3 +27,57 @@ function login() {
       alert(error.message);
     });
 }
+
+function signup() {
+  const email = document.getElementById("signup-email").value;
+  const password = document.getElementById("signup-password").value;
+
+  auth.createUserWithEmailAndPassword(email, password)
+    .then((userCredential) => {
+      const user = userCredential.user;
+
+      // Save user data to Firestore
+      return db.collection("users").doc(user.uid).set({
+        email: email,
+        points: 0
+      });
+    })
+    .then(() => {
+      alert("Account created successfully!");
+      window.location.href = "login.html";
+    })
+    .catch((error) => {
+      alert(error.message);
+    });
+}
+
+function postSkill() {
+
+  const skillName = document.getElementById("skillName").value;
+  const days = document.getElementById("days").value;
+  const time = document.getElementById("time").value;
+  const desc = document.getElementById("desc").value;
+
+  const user = auth.currentUser;
+
+  if (!user) {
+    alert("Please login first.");
+    return;
+  }
+
+  db.collection("skills").add({
+      userID: user.uid,
+      skillName: skillName,
+      days: days,
+      time: time,
+      description: desc
+  })
+  .then(() => {
+      alert("Skill posted successfully!");
+      window.location.href = "dashboard.html";
+  })
+  .catch(error => {
+      alert(error.message);
+  });
+}
+
